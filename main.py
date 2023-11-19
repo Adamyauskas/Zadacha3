44619bb8e57c0588480e45f4217e1cb2d0e20979
Adamyauskas <pbabin558@yandex.ru>
2023-11-19 13:18:59 +0300

    zadacha3

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import sys
 from random import randint
-from PyQt5 import uic
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtCore
 
 
-class Circles(QMainWindow):
+class Ui_Circles(object):
+    def setupUi(self, Circles):
+        Circles.setObjectName("Circles")
+        Circles.resize(615, 502)
+        self.centralwidget = QtWidgets.QWidget(Circles)
+        self.centralwidget.setObjectName("centralwidget")
+        self.make_circle = QtWidgets.QPushButton(self.centralwidget)
+        self.make_circle.setGeometry(QtCore.QRect(270, 370, 75, 23))
+        self.make_circle.setObjectName("make_circle")
+        Circles.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(Circles)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 615, 21))
+        self.menubar.setObjectName("menubar")
+        Circles.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(Circles)
+        self.statusbar.setObjectName("statusbar")
+        Circles.setStatusBar(self.statusbar)
+
+        self.retranslateUi(Circles)
+        QtCore.QMetaObject.connectSlotsByName(Circles)
+
+    def retranslateUi(self, Circles):
+        _translate = QtCore.QCoreApplication.translate
+        Circles.setWindowTitle(_translate("Circles", "MainWindow"))
+        self.make_circle.setText(_translate("Circles", "Круги"))
+
+
+class Circles(QMainWindow, Ui_Circles):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.make_circle.clicked.connect(self.paint)
 
@@ -27,11 +53,11 @@ class Circles(QMainWindow):
 
     def draw_circle(self, qp):
         r = randint(10, 200)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 256), randint(0, 256), randint(0, 256)))
         qp.drawEllipse(100, 100, r, r)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 256), randint(0, 256), randint(0, 256)))
         qp.drawEllipse(400, 100, r, r)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 256), randint(0, 256), randint(0, 256)))
         qp.drawEllipse(250, 200, r, r)
 
 
